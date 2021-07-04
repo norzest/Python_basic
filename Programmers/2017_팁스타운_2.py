7a3a6cddd651585a7f40d4ed8eeca4b3328c22b9
영효 심 <dark1203@naver.com>
2021-07-04 22:41:22 +0900

    2021-07-04

---
+++ Programmers/2017_팁스타운_2.py
@@ -0,0 +1,10 @@
+# 2017 팁스타운 - 예상 대진표
+
+def solution(n, a, b):
+    answer = 0
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution(8, 4, 7))
--- test.py
+++ test.py
@@ -1,14 +1,31 @@
-# 연습문제 - 나누어 떨어지는 숫자 배열
 
-def solution(arr, divisor):
-    answer = [num for num in arr if num % divisor == 0]
-    return sorted(answer) if answer else [-1]
+def solution(maps):
+    answer = -1
+    n, m = len(maps[0]), len(maps)  # 가로, 세로
+    visited = [[0] * n for _ in range(m)]
+    dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]  # 상하좌우
+    visited[0][0] = 1
+    queue = [(0, 0)]
+
+    while queue:
+        x, y = queue.pop(0)
+
+        if x == n - 1 and y == m - 1:
+            answer = visited[x][y]
+            break
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if visited[nx][ny] == 0 and maps[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
+
+    return answer
 
 
 if __name__ == "__main__":
-    print(solution([5, 9, 7, 10], 5))
-    print('-------------')
-    print(solution([2, 36, 1, 3], 1))
-    print('-------------')
-    print(solution([3, 2, 6], 10))
-    print('-------------')
+    print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
+    print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
