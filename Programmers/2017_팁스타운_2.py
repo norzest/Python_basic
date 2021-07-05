53280fd7683e209f86a25e3391f0dc1f4626ea25
영효 심 <dark1203@naver.com>
2021-07-05 17:39:14 +0900

    2021-07-05

--- Programmers/2017_팁스타운_2.py
+++ Programmers/2017_팁스타운_2.py
@@ -1,10 +1,38 @@
 # 2017 팁스타운 - 예상 대진표
 
 def solution(n, a, b):
-    answer = 0
+    def bs(le, num):
+        right = le
+        left = 1
+        count = 1
+        while left <= right:
+            mid = (right + left) // 2
+            #print(mid, end=' ')
+            if mid == num:
+                break
 
-    return answer
+            if mid > num:
+                right = mid - 1
+
+            if mid < num:
+                left = mid + 1
+
+            count += 1
+        #print()
+        return count
+
+    x, y = bs(n, a), bs(n, b)
+    print(x, y)
+
+    return max(x, y)
 
 
 if __name__ == "__main__":
     print(solution(8, 4, 7))
+    print('---')
+    print(solution(256, 46, 243))
+    print('---')
+    print(solution(2, 2, 1))
+    print('---')
+    print(solution(16, 14, 16))
+
--- test.py
+++ test.py
@@ -1,31 +1,33 @@
+# 2017 팁스타운 - 예상 대진표
 
-def solution(maps):
-    answer = -1
-    n, m = len(maps[0]), len(maps)  # 가로, 세로
-    visited = [[0] * n for _ in range(m)]
-    dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]  # 상하좌우
-    visited[0][0] = 1
-    queue = [(0, 0)]
+def solution(n, a, b):
+    def bs(le, num):
+        right = le
+        left = 1
+        count = 1
+        while left <= right:
+            mid = (right + left) // 2
+            print(mid, end=' ')
+            if mid == num:
+                break
 
-    while queue:
-        x, y = queue.pop(0)
+            if mid > num:
+                right = mid - 1
 
-        if x == n - 1 and y == m - 1:
-            answer = visited[x][y]
-            break
+            if mid < num:
+                left = mid + 1
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            count += 1
+        print()
+        return count
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if visited[nx][ny] == 0 and maps[nx][ny] == 1:
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx, ny))
+    x, y = bs(n, a), bs(n, b)
+    print(x, y)
 
-    return answer
+    return max(x, y)
 
 
 if __name__ == "__main__":
-    print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
-    print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
+    print(solution(8, 4, 7))
+    print('---')
+    print(solution(256, 46, 243))
