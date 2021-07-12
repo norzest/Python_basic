069f839754c6b86abf35ba6b1c5c7e58e33c0877
영효 심 <dark1203@naver.com>
2021-07-12 17:18:45 +0900

    2021-07-12 no f

--- Programmers/2021_KAKAO_BLIND_RECRUITMENT_2.py
+++ Programmers/2021_KAKAO_BLIND_RECRUITMENT_2.py
@@ -1,14 +1,30 @@
 # 2021 KAKAO BLIND RECRUITMENT - 메뉴 리뉴얼
 
+from itertools import combinations
+from collections import Counter
+
+
 def solution(orders, course):
     answer = []
 
-    
+    for i in course:
+        comb = []
 
-    return answer
+        # 각각의 문자열에서 i개를 뽑을 때의 조합을 추가
+        for order in orders:
+            comb += combinations(sorted(order), i)
+
+        if comb:
+            # 결과로 나온 조합 리스트를 다시 각각의 나온 횟수로 정리한 객체 생성
+            cnt_comb = Counter(comb)
+            # 가장 나온횟수가 많은 조합을 answer 에 추가
+            answer += [k for k, v in cnt_comb.items() \
+                       if v > 1 and v == max(cnt_comb.values())]
+
+    return [''.join(i) for i in sorted(answer)]
 
 
 if __name__ == "__main__":
     print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
     print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
-    print(solution(	["XYZ", "XWY", "WXA"], [2, 3, 4]))
+    print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
--- test.py
+++ test.py
@@ -1,33 +1,28 @@
-# 2017 팁스타운 - 예상 대진표
+from itertools import combinations
+from collections import Counter
 
-def solution(n, a, b):
-    def bs(le, num):
-        right = le
-        left = 1
-        count = 1
-        while left <= right:
-            mid = (right + left) // 2
-            print(mid, end=' ')
-            if mid == num:
-                break
 
-            if mid > num:
-                right = mid - 1
+def solution(orders, course):
+    answer = []
 
-            if mid < num:
-                left = mid + 1
+    for i in course:
+        comb = []
 
-            count += 1
-        print()
-        return count
+        # 각각의 문자열에서 i개를 뽑을 때의 조합을 추가
+        for order in orders:
+            comb += combinations(sorted(order), i)
 
-    x, y = bs(n, a), bs(n, b)
-    print(x, y)
+        if comb:
+            # 결과로 나온 조합 리스트를 다시 각각의 나온 횟수로 정리한 객체 생성
+            cnt_comb = Counter(comb)
+            # 가장 나온횟수가 많은 조합을 answer 에 추가
+            answer += [k for k, v in cnt_comb.items() \
+                       if v > 1 and v == max(cnt_comb.values())]
 
-    return max(x, y)
+    return [''.join(i) for i in sorted(answer)]
 
 
 if __name__ == "__main__":
-    print(solution(8, 4, 7))
-    print('---')
-    print(solution(256, 46, 243))
+    print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
+    print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
+    print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
