cd9ea39a85071d2af56fe8f35b62c25556b573f0
영효 심 <dark1203@naver.com>
2021-07-14 16:32:46 +0900

    2021-07-14

--- test.py
+++ test.py
@@ -3,37 +3,49 @@
 def solution(rows, columns, queries):
     answer = []
     # rows * columns 행렬
-    arr = [[i + (j*rows) for i in range(1, rows+1)] for j in range(columns)]
-    for i in arr:
-        print(i)
+    arr = [[i + (j*rows) for i in range(1, columns+1)] for j in range(rows)]
 
     for q in queries:
         # 현재 가리키는 위치(숫자를 바꿀 예정인 위치)
         pointer = [q[0] - 1, q[1] - 1]
+        # 숫자를 이동시키면서 임시 저장할 변수
+        temp = arr[pointer[0]][pointer[1]]
+        # 자리가 바뀐 숫자들 중 가장 작은 수를 찾기 위한 임시 배열
+        answer_temp = [temp]
         # 가로, 세로 이동할 횟수
         h, v = q[3] - 1 - pointer[1], q[2] - 1 - pointer[0]
 
+        # 오른쪽으로 이동
         for i in range(h):
-            print(arr[pointer[0]][pointer[1]])
             pointer[1] += 1
+            arr[pointer[0]][pointer[1]], temp = temp, arr[pointer[0]][pointer[1]]
+            answer_temp.append(temp)
 
+        # 아래로 이동
         for i in range(v):
-            print(arr[pointer[0]][pointer[1]])
             pointer[0] += 1
+            arr[pointer[0]][pointer[1]], temp = temp, arr[pointer[0]][pointer[1]]
+            answer_temp.append(temp)
 
+        # 왼쪽으로 이동
         for i in range(h):
-            print(arr[pointer[0]][pointer[1]])
             pointer[1] -= 1
+            arr[pointer[0]][pointer[1]], temp = temp, arr[pointer[0]][pointer[1]]
+            answer_temp.append(temp)
 
+        # 위로 이동
         for i in range(v):
-            print(arr[pointer[0]][pointer[1]])
             pointer[0] -= 1
+            arr[pointer[0]][pointer[1]], temp = temp, arr[pointer[0]][pointer[1]]
+            answer_temp.append(temp)
 
-        print('--')
+        # 가장 작은 수 저장
+        answer.append(min(answer_temp))
+
     return answer
 
 
 if __name__ == "__main__":
     print(solution(6, 6, [[2, 2, 5, 4], [3, 3, 6, 6], [5, 1, 6, 3]]))
     print(solution(3, 3, [[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]]))
-    # print(solution(100, 97, [[1, 1, 100, 97]]))
+    print(solution(100, 97, [[1, 1, 100, 97]]))
--- test2.py
+++ test2.py
@@ -1,34 +1,4 @@
-# 2017 팁스타운 - 예상 대진표
-
-def solution(n, a, b):
-    p = 2
-    ac, bc = 0, 0
-
-    count = 0
-    while p <= n:
-        count += 1
-
-        if a <= p and ac == 0:
-            ac = count
-
-        if b <= p and bc == 0:
-            bc = count
-
-        p *= 2
-
-    print(ac, bc)
-
-    return 0
-
-
-if __name__ == "__main__":
-    print(solution(8, 4, 7))
-    print('---')
-    print(solution(256, 46, 243))
-    print('---')
-    print(solution(2, 2, 1))
-    print('---')
-    print(solution(16, 14, 16))
-
-
-
+a = 3
+b = 4
+a, b = b, a
+print(a, b)
