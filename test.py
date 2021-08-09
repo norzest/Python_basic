57ea8e8f5646ef81fafa1b0701b0e6222568be7d
영효 심 <dark1203@naver.com>
2021-08-09 23:13:39 +0900

    2021-08-09

--- test.py
+++ test.py
@@ -1,82 +1,19 @@
-# 2021 카카오 채용연계형 인턴십 - 거리두기 확인하기
-
-# BFS 로 풀어보기
-from itertools import product
+# 2019 KAKAO BLIND RECRUITMENT - 후보키
 
 
-def solution(places):
-    answer = []
-    plc_len = 5
+def solution(relation):
+    answer = 0
+    relation_len = len(relation[0])
+    attr = [[r[i] for r in relation] for i in range(relation_len)]
 
-    for place in places:
-        place = list(map(list, place))
-        result = 1
-
-        for x, y in product(range(plc_len), range(plc_len)):
-            breaker = False
-            # P 찾기
-            if place[x][y] == 'P':
-                # 맨해튼 거리 1 확인
-                dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-                for i in range(4):
-                    tx, ty = x + dx[i], y + dy[i]
-                    if tx >= 0 and ty >= 0:
-                        try:
-                            if place[tx][ty] == 'P':
-                                result = 0
-                                breaker = True
-                        except IndexError:
-                            pass
-                # 대각 확인
-                dx, dy = [1, 1, -1, -1], [1, -1, 1, -1]
-                for i in range(4):
-                    tx, ty = x + dx[i], y + dy[i]
-                    if tx >= 0 and ty >= 0:
-                        try:
-                            if place[tx][ty] == 'P':
-                                if place[x][ty] == 'O' or place[tx][y] == 'O':
-                                    result = 0
-                                    breaker = True
-                        except IndexError:
-                            pass
-                # 맨해튼 거리 2 확인
-                # 세로 POP 일 경우
-                try:
-                    if x - 2 >= 0 and place[x - 2][y] == 'P' and place[x - 1][y] == 'O':
-                        result = 0
-                        breaker = True
-                except IndexError:
-                    pass
-                try:
-                    if place[x + 2][y] == 'P' and place[x + 1][y] == 'O':
-                        result = 0
-                        breaker = True
-                except IndexError:
-                    pass
-                # 가로 POP 일 경우
-                try:
-                    if y - 2 >= 0 and place[x][y - 2] == 'P' and place[x][y - 1] == 'O':
-                        result = 0
-                        breaker = True
-                except IndexError:
-                    pass
-                try:
-                    if place[x][y + 2] == 'P' and place[x][y + 1] == 'O':
-                        result = 0
-                        breaker = True
-                except IndexError:
-                    pass
-
-            if breaker:
-                break
-
-        answer.append(result)
-
+    print(attr)
     return answer
 
 
 if __name__ == "__main__":
-    print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
-                    ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
-                    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"], ["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"],
-                    ["OOOOP", "OOOOO", "OOOOP", "OOOOO", "OOOOO"]]))
+    print(solution([["100", "ryan", "music", "2"],
+                    ["200", "apeach", "math", "2"],
+                    ["300", "tube", "computer", "3"],
+                    ["400", "con", "computer", "4"],
+                    ["500", "muzi", "music", "3"],
+                    ["600", "apeach", "music", "2"]]))
--- test3.py
+++ test3.py
@@ -1,6 +1,22 @@
-try:
-    print(4/4)
-    print(4/0)
-    print(3)
-except:
-    pass
+def drawing_pyramid(n):
+    for i in range(n):
+        print(' ' * (n - i - 1), '*' * (1 + (i * 2)))
+
+
+inp = 1
+while inp > 0:
+    try:
+        inp = int(input('양의 정수만 입력: '))
+        drawing_pyramid(inp)
+    except ValueError:
+        print('양의 정수를 입력해 주세요')
+
+##############################
+
+while True:
+    inp = input('양의 정수만 입력: ')
+    if inp.isdigit():
+        drawing_pyramid(int(inp))
+    else:
+        print('양의 정수를 입력해 주세요')
+        break
