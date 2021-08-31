f9a08e6f644de86b094fbef9c2b0195e51075c42
영효 심 <dark1203@naver.com>
2021-08-31 16:45:08 +0900

    2021-08-31

--- Programmers/2021_KAKAO_BLIND_RECRUITMENT_3.py
+++ Programmers/2021_KAKAO_BLIND_RECRUITMENT_3.py
@@ -22,7 +22,7 @@ def solution(info, query):
                     applicant[temp].append(value)
                 else:
                     applicant[temp] = [value]
-    
+
     # 각 키의 값들을 정렬
     for key in applicant.keys():
         applicant[key].sort()
@@ -54,4 +54,9 @@ def solution(info, query):
 
 
 if __name__ == "__main__":
-    print(solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
+    print(solution(
+        ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150",
+         "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"],
+        ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
+         "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100",
+         "- and - and - and - 150"]))
