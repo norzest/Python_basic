72d0ea306d0ade21f6d20ca04513173e1e52302e
영효 심 <dark1203@naver.com>
2021-09-04 19:43:00 +0900

    2021-09-04

--- test.py
+++ test.py
@@ -1,53 +1,23 @@
-# 2018 KAKAO BLIND RECRUITMENT - 프렌즈4블록
+# 월간 코드 챌린지 시즌 2 - 2개 이하로 다른 비트
 
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        bn = format(number, 'b')
 
-def solution(m, n, board):
-    board = [list(_) for _ in board]
-    answer = 0
-
-    while True:
-        # 같은 모양의 2x2 블록을 찾으면 1로 표시
-        removed = [[0] * n for _ in range(m)]
-
-        for i in range(m - 1):
-            for j in range(n - 1):
-                # 아직 지워지지 않은 블록이면
-                if board[i][j] != 0:
-                    # 2x2 블록이면
-                    if board[i][j] == board[i][j+1]:
-                        if board[i][j] == board[i+1][j]:
-                            if board[i][j] == board[i+1][j+1]:
-                                # removed 배열에 1로 표시
-                                removed[i][j], removed[i][j+1], removed[i+1][j], removed[i+1][j+1] = 1, 1, 1, 1
-
-        # 지워진 블록의 갯수
-        count = 0
-        for i in range(m):
-            count += sum(removed[i])
-
-        # 지워진 블록이 없을 경우 종료
-        if count == 0:
-            break
-
-        answer += count
-
-        # removed 에 표시됀 블록 board 에서 제거(0 으로 표시)
-        for i in range(m - 1, -1, -1):
-            for j in range(n):
-                if removed[i][j] == 1:
-                    temp = i - 1
-
-                    while temp >= 0 and removed[temp][j] == 1:
-                        temp -= 1
-                    if temp < 0:
-                        board[i][j] = 0
-                    else:
-                        board[i][j] = board[temp][j]
-                        removed[temp][j] = 1
+        if bn[-1] == '0':
+            answer.append(int(''.join([bn[:-1], '1']), 2))
+        else:
+            bn = list(bn[::-1])
+            if '0' not in bn:
+                bn.append('0')
+            i = bn.index('0')
+            bn[i] = '1'
+            bn[i-1] = '0'
+            answer.append(int(''.join(bn[::-1]), 2))
 
     return answer
 
 
 if __name__ == "__main__":
-    print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))
-    print(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
+    print(solution([2, 7]))
