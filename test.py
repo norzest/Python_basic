97584b119edae68df3b1eb656c11f4fcca5a0ecb
영효 심 <dark1203@naver.com>
2021-09-29 19:30:40 +0900

    2021-09-29

--- test.py
+++ test.py
@@ -2,46 +2,38 @@
 
 
 def solution(m, musicinfos):
-    answer = []
+    answer = "(None)"
+    runtime = 99999
 
-    # '#'이 붙은 음을 소문자로 치환
-    m = list(m)
-    for i in range(len(m)):
-        if m[i] == '#':
-            m[i-1] = m[i-1].lower()
-    m = ''.join(m)
-    m = m.replace('#', "")
+    # m에서 '#'이 붙은 음을 소문자로 치환
+    token = ['C#', 'D#', 'F#', 'G#', 'A#']
+    for t in token:
+        m = m.replace(t, t[0].lower())
 
     for musicInfo in musicinfos:
         # 곡의 정보들을 , 를 구분으로 분할
         info = musicInfo.split(",")
         # 곡의 재생 시간
         run = int(info[1][3:]) - int(info[0][3:])
-        while run < 0:
-            run += 60
+        # 11:50 부터 13:10 일 경우 시간당 60분을 추가해야 하므로
         diff = int(info[1][:2]) - int(info[0][:2])
-        if diff > 1:
-            run += 60 * (diff - 1)
+        run += diff*60
 
-        # 악보를 음 별로 분할
-        lst = []
-        for i in info[3]:
-            if i != '#':
-                lst.append(i)
-            else:
-                # '#'이 붙은 음을 소문자로 치환
-                lst[-1] = lst[-1].lower()
-        lst = ''.join(lst)
+        # 방송된 곡에서 '#'이 붙은 음을 소문자로 치환
+        for t in token:
+            info[3] = info[3].replace(t, t[0].lower())
         # 곡의 길이
-        n = len(lst)
+        n = len(info[3])
         # 실제 재생한 악보
-        play = ''.join((lst * (run // n)) + lst[:run % n])
+        play = ''.join((info[3] * (run // n)) + info[3][:run % n])
 
+        # 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
+        # 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
         if m in play:
-            answer.append(info[2])
+            if answer == "(None)" or runtime < run:
+                answer = info[2]
+                runtime = run
 
-    # 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
-    # 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
     return answer
 
 
@@ -49,4 +41,4 @@ if __name__ == "__main__":
     print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
     print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
     print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
-    # print(solution("ABC", ["10:50,12:10,HELLO,C#DEFGAB"]))
+    print(solution("ABC", ["10:50,12:10,HELLO,C#DEFGAB"]))
