a6650dd3414667be491354ebbdb583edd4bea359
영효 심 <dark1203@naver.com>
2021-10-01 17:37:14 +0900

    2021-10-01

--- test.py
+++ test.py
@@ -2,15 +2,23 @@
 
 
 def solution(board):
-    answer = 0
+    # 가장 큰 정사각형의 변의 길이
+    max_num = 0
+    x = len(board)
+    y = len(board[0])
+    if x <= 1 or y <= 1:
+        return 1
 
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            print(board[i][j], end='')
+    for i in range(1, x):
+        for j in range(1, y):
+            if board[i][j] >= 1:
+                # 현재 칸의 위, 왼쪽, 왼쪽 위 중 최소값
+                min_num = min(board[i-1][j], board[i][j-1], board[i-1][j-1])
+                # min_num + 1 을 현재 칸에 대입
+                board[i][j] = min_num + 1
+                max_num = max(min_num + 1, max_num)
 
-        print()
-
-    return answer
+    return max_num * max_num
 
 
 if __name__ == "__main__":
