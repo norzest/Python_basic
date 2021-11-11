c44abff746d9e15729977612307484d185c66c56
영효 심 <dark1203@naver.com>
2021-11-11 22:51:36 +0900

    2021-11-11

--- Programmers/2020_KAKAO_BLIND_RECRUITMENT_3.py
+++ Programmers/2020_KAKAO_BLIND_RECRUITMENT_3.py
@@ -1,45 +1,56 @@
 # 2020 KAKAO BLIND RECRUITMENT - 자물쇠와 열쇠
 
 
-def rotation(key, key_len):  # key 90도 회전
-    temp = [[0] * key_len for _ in range(key_len)]
-    for i in range(key_len):
-        for j in range(key_len):
-            temp[j][key_len-1-i] = key[i][j]
-    return temp
+# 키값 회전
+def rotate(key):
+    m = len(key)
+    result = [[0] * m for _ in range(m)]
+    for i in range(m):
+        for j in range(m):
+            result[j][m - 1 - i] = key[i][j]
+    return result
 
 
-def match(key, lock, lock_len):  # 자물쇠와 키 매칭
-    count = 0
+# 키가 자물쇠에 맞는지 확인
+def check(lock_padding):
+    lock_length = len(lock_padding) // 3
+    for i in range(lock_length, lock_length*2):
+        for j in range(lock_length, lock_length*2):
+            if lock_padding[i][j] != 1:
+                return False
+    return True
 
-    for i in range(lock_len, lock_len * 2):
-        for j in range(lock_len, lock_len * 2):
-            print(lock[i][j], end='')
-        print()
 
-    print('-----')
+# 키를 자물쇠에 넣었을때
+def keyIn(lock_padding, key):
+    n = len(lock_padding) // 3
+    for x in range(n*2):
+        for y in range(n*2):
+            for i in range(len(key)):
+                for j in range(len(key)):
+                    lock_padding[x + i][y + j] += key[i][j]
+            if check(lock_padding):
+                return True
+            for i in range(len(key)):
+                for j in range(len(key)):
+                    lock_padding[x + i][y + j] -= key[i][j]
     return False
 
 
 def solution(key, lock):
-    answer = False
-    key_len = len(key)
-    lock_len = len(lock)
-
-    # 자물쇠 확장
-    ex_lock = [[0] * (lock_len * 3) for _ in range(lock_len * 3)]
-    for i in range(lock_len):
-        for j in range(lock_len):
-            ex_lock[i + lock_len][j + lock_len] = lock[i][j]
-
-    cnt = 0
-    while cnt < 4:
-        key = rotation(key, key_len)
-        answer = match(key, ex_lock, lock_len)
-        if answer:
+    n = len(lock)
+    rotate_count = 0
+    lock_padding = [[0] * (n*3) for _ in range(n*3)]  # 새로운 자물쇠 만들기
+    for i in range(n):
+        for j in range(n):
+            lock_padding[i + n][j + n] = lock[i][j]
+    while True:
+        answer = keyIn(lock_padding, key)
+        if answer or rotate_count == 3:
             break
-        cnt += 1
-
+        else:
+            key = rotate(key)
+            rotate_count += 1
     return answer
 
 
--- test.py
+++ test.py
@@ -1,49 +1,16 @@
-# 2020 KAKAO BLIND RECRUITMENT - 자물쇠와 열쇠
+# 연습문제 - 거스름돈
 
 
-def rotation(key, key_len):  # key 90도 회전
-    temp = [[0] * key_len for _ in range(key_len)]
-    for i in range(key_len):
-        for j in range(key_len):
-            temp[j][key_len-1-i] = key[i][j]
-    return temp
+def solution(n, money):
+    dp = [1] + [0] * n
 
+    for m in money:
+        for i in range(m, n + 1):
+            if i >= m:
+                dp[i] += dp[i - m]
 
-def match(key, lock, lock_len):  # 자물쇠와 키 매칭
-    count = 0
-
-    for i in range(lock_len, lock_len * 2):
-        for j in range(lock_len, lock_len * 2):
-            print(lock[i][j] + key[i - lock_len][j - lock_len], end="")
-            if lock[i][j] + key[i - lock_len][j - lock_len] == 1:
-                count += 1
-        print()
-    print(count)
-    return count == lock_len * lock_len
-
-
-def solution(key, lock):
-    answer = False
-    key_len = len(key)
-    lock_len = len(lock)
-
-    # 자물쇠 확장
-    ex_lock = [[0] * (lock_len * 3) for _ in range(lock_len * 3)]
-    for i in range(lock_len):
-        for j in range(lock_len):
-            ex_lock[i + lock_len][j + lock_len] = lock[i][j]
-
-    cnt = 0
-    while cnt < 4:
-        key = rotation(key, key_len)
-        answer = match(key, ex_lock, lock_len)
-        if answer:
-            break
-        cnt += 1
-
-    return answer
+    return dp[n] % 1000000007
 
 
 if __name__ == "__main__":
-    print(solution(	[[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
-    print(solution(	[[0, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 0, 1]], [[1, 1, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 0]]))
+    print(solution(5, [1, 2, 5]))
