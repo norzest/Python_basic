b3d92604298052b8843abb8dafb8674af8565ef4
영효 심 <dark1203@naver.com>
2021-12-23 19:55:31 +0900

    2021-12-23

--- test.py
+++ test.py
@@ -1,29 +1,9 @@
-# 2021 카카오 채용연계형 인턴쉽 - 표 편집
+# 1234 제목
 
+n = range(int(input()))
 
-def solution(n, k, cmd):
-    origin = [[i, i] for i in range(n)]  # 비교할 원본
-    arr = origin.copy()  # 수정할 표
-    deleted = []  # 삭제된 행
-
-    for c in cmd:
-        if c[0] == 'U':
-            k -= int(c[2])
-        elif c[0] == 'D':
-            k += int(c[2])
-        elif c[0] == 'C':
-            temp = []
-            deleted.append(temp)
-            print(arr)
-        elif c[0] == 'Z':
-            print(arr)
-
-        print('---')
-
-    answer = '---'
-    return answer
-
-
-if __name__ == "__main__":
-    print(solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"]))
-    print(solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"]))
+for i in n:
+    ip = list(map(int, input().split()))
+    h = ip[2] % ip[0]
+    w = ip[2] // ip[0] + 1
+    print(h * 100, w)
