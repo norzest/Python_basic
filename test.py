f9aa94cde4877b37671c89a0ee45dfd403dce05e
영효 심 <dark1203@naver.com>
2021-12-29 17:42:08 +0900

    2021-12-29

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-# 1259 팰린드롬수
+# 2839 설탕 배달
 
-while True:
-    ip = input()
+import sys
 
-    if ip == '0':
-        break
 
-    if ip == ip[::-1]:
-        print('yes')
+def solution():
+    N = int(sys.stdin.readline())
+
+    cnt = 0
+    while N >= 0:
+        if N % 5 == 0:
+            cnt += (N // 5)
+            print(cnt)
+            break
+        N -= 3
+        cnt += 1
     else:
-        print('no')
+        print(-1)
+
+
+if __name__ == '__main__':
+    solution()
