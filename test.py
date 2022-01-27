7f43cc20313436d93a24e6869781bfa8c1ec36b5
영효 심 <dark1203@naver.com>
2022-01-27 19:50:53 +0900

    2022-01-27

--- test.py
+++ test.py
@@ -1,36 +1,25 @@
-# 11723 집합
+# 1620 나는야 포켓몬 마스터 이다솜
 
 from sys import stdin
 
 
 def solution():
     sr = stdin.readline
-    m = int(sr())
+    n, m = map(int, sr().split())
 
-    s = set()
+    l = list()
+    d = dict()
+    for i in range(n):
+        temp = sr().rstrip()
+        l.append(temp)
+        d[temp] = i + 1
+
     for i in range(m):
-        cal = sr().split()
-
-        if len(cal) == 1:
-            if cal[0] == 'all':
-                s = set(range(1, 21))
-            elif cal[0] == 'empty':
-                s = set()
-            continue
-
-        cal[1] = int(cal[1])
-
-        if cal[0] == 'add':
-            s.add(cal[1])
-        elif cal[0] == 'remove':
-            s.discard(cal[1])
-        elif cal[0] == 'check':
-            print(1 if cal[1] in s else 0)
-        elif cal[0] == 'toggle':
-            if cal[1] in s:
-                s.discard(cal[1])
-            else:
-                s.add(cal[1])
+        p = sr().rstrip()
+        if p.isalpha():
+            print(d[p])
+        else:
+            print(l[int(p) - 1])
 
 
 if __name__ == '__main__':
