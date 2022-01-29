1187693d9973c33b5f3f9b174277d4eb9c607de5
영효 심 <dark1203@naver.com>
2022-01-29 23:59:53 +0900

    2022-01-29

--- test.py
+++ test.py
@@ -7,19 +7,6 @@ def solution():
     sr = stdin.readline
     n, m = map(int, sr().split())
 
-    l = list()
-    d = dict()
-    for i in range(n):
-        temp = sr().rstrip()
-        l.append(temp)
-        d[temp] = i + 1
-
-    for i in range(m):
-        p = sr().rstrip()
-        if p.isalpha():
-            print(d[p])
-        else:
-            print(l[int(p) - 1])
 
 
 if __name__ == '__main__':
