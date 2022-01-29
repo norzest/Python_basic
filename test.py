abf2c73b1a54200b0c0520fd00660046afbc76b4
영효 심 <dark1203@naver.com>
2022-01-30 00:30:17 +0900

    2022-01-30

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-# 1620 나는야 포켓몬 마스터 이다솜
+# 1676 팩토리얼 0의 개수
 
 from sys import stdin
 
 
 def solution():
-    sr = stdin.readline
-    n, m = map(int, sr().split())
+    sr = stdin.readline()
+    n = int(sr)
+    a = 1
+    for i in range(1, n + 1):
+        a = a * i
+    a = str(a)
 
+    cnt = 0
+    for i in reversed(range(0, len(a))):
+        if a[i] == '0':
+            cnt += 1
+        else:
+            print(cnt)
+            break
 
 
 if __name__ == '__main__':
