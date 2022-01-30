5ff51345683c81a0e24806b4f9bfe158986db4da
영효 심 <dark1203@naver.com>
2022-01-31 00:16:37 +0900

    2022-01-31

--- test.py
+++ test.py
@@ -1,23 +1,26 @@
-# 1676 팩토리얼 0의 개수
+# 1764 듣보잡
 
 from sys import stdin
 
 
 def solution():
-    sr = stdin.readline()
-    n = int(sr)
-    a = 1
-    for i in range(1, n + 1):
-        a = a * i
-    a = str(a)
+    sr = stdin.readline
+    n, m = map(int, sr().split(" "))
+    dd = set()
+    for i in range(n):
+        dd.add(sr().rstrip())
 
     cnt = 0
-    for i in reversed(range(0, len(a))):
-        if a[i] == '0':
+    bd = set()
+    for i in range(m):
+        temp = sr().rstrip()
+        if temp in dd:
+            bd.add(temp)
             cnt += 1
-        else:
-            print(cnt)
-            break
+
+    print(cnt)
+    for b in sorted(bd):
+        print(b)
 
 
 if __name__ == '__main__':
